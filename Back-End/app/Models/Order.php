542505f19f1147ac3e2e6d40e4c91575ecbfb563
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-08-07 00:00:45 +0300

    casts the timestamps to date

--- Back-End/app/Models/Order.php
+++ Back-End/app/Models/Order.php
@@ -11,12 +11,17 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
+    protected $casts = [
+        'ordered_date' => 'datetime',
+        'shipped_date' => 'datetime',
+        'delivered_date' => 'datetime',
+    ];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-    public function orderItem():BelongsTo
+    public function orderItem(): BelongsTo
     {
         return $this->belongsTo(orderItem::class);
     }
