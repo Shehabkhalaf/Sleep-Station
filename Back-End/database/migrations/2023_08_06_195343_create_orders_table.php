b2ba80fbe931d817e750c8a6efd41d8d733be6ba
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-08-06 23:16:04 +0300

    Migrations and models are created

---
+++ Back-End/database/migrations/2023_08_06_195343_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->enum('status',['shipped','delivered','in consider'])->default('in consider');
+            $table->enum('paid',['cash','paid'])->default('cash');
+            $table->timestamp('ordered_date');
+            $table->timestamp('shipped_date')->nullable();
+            $table->timestamp('delivered_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
