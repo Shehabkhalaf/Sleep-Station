49165f18585fba9fc1019f27cc002bf5cd934249
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2023-08-12 13:57:10 +0300

    up

--- project/js/product.js
+++ project/js/product.js
@@ -28,17 +28,17 @@ fetch(urlApi).then(
 
 // Add Products
 function addProduct(data) {
-    sliderImages.innerHTML = `
-        ${data.color.map((colorType, index) => ` <div ${index === 0 ? `class="carousel-item active" ` : "carousel-item"}>
-            <img src=${data.images[index]} class="d-block" alt="...">
-        </div>
-        `).join(" ")}
-    `
+    // sliderImages.innerHTML = `
+    //     ${data.color.map((colorType, index) => ` <div ${index === 0 ? `class="carousel-item active" ` : "carousel-item"}>
+    //         <img src=${data.images[index]} class="d-block" alt="...">
+    //     </div>
+    //     `).join(" ")}
+    // `
     detailsContainer.innerHTML = `
             <a href="./products.html" class="buttonStyleBack" id="back-btnn"><i class="fa-solid fa-arrow-left"></i> Back To All Products</a>
             <h2 class="mt-5">${data.product_name}</h2>
-            <p class="price" id="discountP">EGP ${data.discount[0]} <del id="priceDel">EGP
-                    ${data.price[0]}</del></p>
+            <p class="price" id="discountP">EGP ${data.price[0]} <del id="priceDel">EGP
+                    ${data.discount[0]}</del></p>
             <div class="description">${data.description}</div>
             <div class="form">
                 <div class="row">
@@ -118,7 +118,7 @@ function showProducts(data) {
 }
 
 
-// // Add Product
+// Add Product
 function addProducts(product) {
     product.products.forEach(element => {
         // create div
