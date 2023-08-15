802eeea63c077f76e774d81b02c87a8b41ecd84e
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2023-08-15 15:32:33 +0300

    up

--- admin/js/deleteproduct.js
+++ admin/js/deleteproduct.js
@@ -2,30 +2,31 @@
 let inputSearch = document.getElementById("searchProduct");
 let buttonSearch = document.getElementById("buttonSearch");
 let showProduct = document.getElementById("showProduct");
-let buttonDelete = document.getElementById("buttonDelete");
 
 // Call Product 
-fetch(`http://127.0.0.1:8000/api/admin/show_product/${id}`)
-    .then((res) => res.json())
-    .then((data) => {
-        showProduct.innerHTML = ''
-        // In Case Fount Or Not fount
-        if (data.status === 404) {
-            showProduct.innerHTML = `
+buttonSearch.addEventListener('click', () => {
+    fetch(`http://127.0.0.1:8000/api/admin/show_product/${buttonSearch.value}`)
+        .then((res) => res.json())
+        .then((data) => {
+            showProduct.innerHTML = ''
+            // In Case Fount Or Not fount
+            if (data.status === 404) {
+                showProduct.innerHTML = `
         <h1 class="text-center" style="color: red;">NOT FOUNT !</h1>
         `
-        } else {
-            showProduct.innerHTML = `
+            } else {
+                showProduct.innerHTML = `
                     <h3 style = "color: var(--primary-color);" > Product Id: <span style="color: #333;">${data.data.product_id}</span> </h3>
                     <h3 style="color: var(--primary-color);">Name Product: <span style="color: #333;">${data.data.product_name}</span></h3>
                     <h3 style="color: var(--primary-color);">Category : <span style="color: #333;">${data.data.category_name}</span></h3>
                     <button class="buttonDelete" id="buttonDelete">Delete</button>
             `
-            // DELETE product
-            document.getElementById("buttonDelete").addEventListener("click", (e) => {
-                const xhr = new XMLHttpRequest();
-                xhr.open('POST', `http://127.0.0.1:8000/api/admin/delete_product/${data.data.product_id}`, true);
-                xhr.send(formData);
-            })
-        }
-    })
+                // DELETE product
+                document.getElementById("buttonDelete").addEventListener("click", (e) => {
+                    const xhr = new XMLHttpRequest();
+                    xhr.open('POST', `http://127.0.0.1:8000/api/admin/delete_product/${data.data.product_id}`, true);
+                    xhr.send(formData);
+                })
+            }
+        })
+})
