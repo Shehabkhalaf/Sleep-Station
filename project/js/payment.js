d5c4ab438619df7fc59cacd6a535c57434e2903e
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2023-08-16 19:36:17 +0300

    up

--- project/js/payment.js
+++ project/js/payment.js
@@ -2,7 +2,7 @@ const products = JSON.parse(localStorage.getItem("products"));
 
 
 
-let listItems = products.map(element => {element.title , element.size , element.color , element.price })
+let listItems = products.map(element => {element.title , element.size , element.color , element.discount })
 
 
 console.log(JSON.stringify(listItems))
