0a5246708167aee57ed81f4197ec274d278d9bff
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2023-08-18 15:13:53 +0300

    up

--- project/js/payment.js
+++ project/js/payment.js
@@ -3,12 +3,11 @@ const userData = JSON.parse(localStorage.getItem("userData"));
 
 
 
-console.log(products)
 let listItems = products.map(element => element.title + "," + element.size + "," + element.color  + "," + element.discount );
 let productss =[];
 
 products.forEach(element => {
-    productss.push({"product_id" : 2  ,"amount" : element.quantity })
+    productss.push({product_id : 2  ,amount : element.quantity })
 });
 
 let totalPrice = products.map(element =>element.discount).reduce((acc, ele) => +acc + +ele);
