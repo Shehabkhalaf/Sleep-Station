6de76bacb9c5ea66756a068f9222944290d9fcb3
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-08-29 21:41:17 +0300

    reject order

--- Back-End/app/Http/Controllers/Admin/OrderController.php
+++ Back-End/app/Http/Controllers/Admin/OrderController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AdminOrders;
 use App\Models\AdminOrder;
+use App\Models\ArabicProduct;
 use App\Models\Order;
+use App\Models\Product;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
@@ -26,6 +28,23 @@ class OrderController extends Controller
     }
     public function rejectOrder(Request $request)
     {
+        $order = AdminOrder::find($request->id);
+        $order_details = json_decode($order->order_details);
+        foreach ($order_details as $order_detail) {
+            $pattern = '/^[\p{Arabic}\s]+$/u';
+            $arr_of_order_detail = explode(',', $order_detail);
+            $amount = $arr_of_order_detail[count($arr_of_order_detail) - 1];
+            if (preg_match($pattern, $arr_of_order_detail[0])) {
+                $arabic_product = ArabicProduct::where('title', $arr_of_order_detail[0])->first();
+                $product = Product::find($arabic_product->product_id);
+                $product->stock = $product->stock + $amount;
+                $product->save();
+            } else {
+                $product = Product::where('title', $arr_of_order_detail[0])->first();
+                $product->stock = $product->stock + $amount;
+                $product->save();
+            }
+        }
         $destroyAdmin = AdminOrder::destroy($request->id);
         $destroyUser = Order::destroy($request->id);
         if ($destroyUser && $destroyAdmin) {
