fe53392365e65f61b0aeae67619f758b642c5fed
Shehab Khalaf <shehabkhalaf7474@gmail.com>
2023-09-01 16:24:17 +0300

    Admin login

--- Back-End/app/Http/Controllers/Admin/AccessController.php
+++ Back-End/app/Http/Controllers/Admin/AccessController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AccessController extends Controller
 {
     use ApiResponse;
     public function login(Request $request)
     {
-        if ($request->username == 'admin@sleepstation.com' && $request->passwrod == 'pH3vyPOzTY5thUHQnlLucUf6zAGyqG') {
-            return $this->JsonResponse(200, 'You are logged in');
+        $admin = Admin::where('email', '=', $request->username)->first();
+        if ($admin) {
+            if (Hash::check($request->password, $admin->password)) {
+                return $this->JsonResponse(200, 'You are logged in');
+            } else {
+                return $this->JsonResponse(401, 'You are not permitted');
+            }
         } else {
             return $this->JsonResponse(401, 'You are not permitted');
         }
